04fd379630a1f6ea0a4e7bdc6bb4e34b388d0b3b
Nhữ Mạnh Cần <maccervn@gmail.com>
2021-06-23 17:32:14 +0700

    add controller about product

---
+++ WebAPI/Controllers/DiscountController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Data;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DiscountController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Discount
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Discount>>> GetDiscount()
+        {
+            return await _context.Discount.ToListAsync();
+        }
+
+        // GET: api/Discount/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Discount>> GetDiscount(int id)
+        {
+            var discount = await _context.Discount.FindAsync(id);
+
+            if (discount == null)
+            {
+                return NotFound();
+            }
+
+            return discount;
+        }
+
+        // PUT: api/Discount/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDiscount(int id, Discount discount)
+        {
+            if (id != discount.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(discount).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DiscountExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Discount
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Discount>> PostDiscount(Discount discount)
+        {
+            _context.Discount.Add(discount);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetDiscount", new { id = discount.ID }, discount);
+        }
+
+        // DELETE: api/Discount/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDiscount(int id)
+        {
+            var discount = await _context.Discount.FindAsync(id);
+            if (discount == null)
+            {
+                return NotFound();
+            }
+
+            _context.Discount.Remove(discount);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool DiscountExists(int id)
+        {
+            return _context.Discount.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ WebAPI/Controllers/ProductDetailsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Data;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductDetailsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductDetailsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ProductDetails
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDetails>>> GetProductDetails()
+        {
+            return await _context.ProductDetails.ToListAsync();
+        }
+
+        // GET: api/ProductDetails/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductDetails>> GetProductDetails(int id)
+        {
+            var productDetails = await _context.ProductDetails.FindAsync(id);
+
+            if (productDetails == null)
+            {
+                return NotFound();
+            }
+
+            return productDetails;
+        }
+
+        // PUT: api/ProductDetails/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProductDetails(int id, ProductDetails productDetails)
+        {
+            if (id != productDetails.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(productDetails).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductDetailsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ProductDetails
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ProductDetails>> PostProductDetails(ProductDetails productDetails)
+        {
+            _context.ProductDetails.Add(productDetails);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProductDetails", new { id = productDetails.ID }, productDetails);
+        }
+
+        // DELETE: api/ProductDetails/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProductDetails(int id)
+        {
+            var productDetails = await _context.ProductDetails.FindAsync(id);
+            if (productDetails == null)
+            {
+                return NotFound();
+            }
+
+            _context.ProductDetails.Remove(productDetails);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProductDetailsExists(int id)
+        {
+            return _context.ProductDetails.Any(e => e.ID == id);
+        }
+    }
+}
